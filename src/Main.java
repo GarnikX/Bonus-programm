4c374d5ccf1bd3d3f6cc31a135336cbe3e99f3c7
Garnik Khachatryan <garniktester@gmail.com>
2021-12-21 18:58:28 +0400

    Code refactored

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
+
     public static void main(String[] args) {
         int clientBalance = 100;
         int replenishmentAmount = 915;
+
         if (replenishmentAmount >= 1000) {
             System.out.println(clientBalance + replenishmentAmount + replenishmentAmount / 100);
         } else {
             System.out.println(clientBalance + replenishmentAmount);
         }
+
     }
 }
